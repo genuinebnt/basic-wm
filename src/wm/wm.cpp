ccb4a03b224c3c192e3b2abedf1862177a2ebbc1
genuine <genuinebnt@gmail.com>
2023-04-28 10:12:11 +0530

    Added functionality to map window and create window frames

--- src/wm/wm.cpp
+++ src/wm/wm.cpp
@@ -18,4 +18,128 @@ WindowManager::WindowManager(Display *display)
 
 WindowManager::~WindowManager() { XCloseDisplay(display_); }
 
-void WindowManager::Run() {}
+void WindowManager::Run() {
+  XSetErrorHandler(&WindowManager::OnWMDectected);
+  XSelectInput(display_, root_,
+               SubstructureRedirectMask | SubstructureNotifyMask);
+  XSync(display_, false);
+
+  if (wm_detected_) {
+    LOG(ERROR) << "Detected another window manager running on display "
+               << XDisplayString(display_);
+    return;
+  }
+
+  XSetErrorHandler(&WindowManager::OnXError);
+
+  for (;;) {
+    XEvent e;
+    XNextEvent(display_, &e);
+
+    switch (e.type) {
+    case CreateNotify:
+      OnCreateNotify(e.xcreatewindow);
+      break;
+    case DestroyNotify:
+      OnDestroyNotify(e.xdestroywindow);
+      break;
+    case ReparentNotify:
+      OnReparentNotify(e.xreparent);
+      break;
+    case ConfigureRequest:
+      OnConfigureRequest(e.xconfigurerequest);
+      break;
+    case ConfigureNotify:
+      OnConfigureNotify(e.xconfigure);
+      break;
+    case MapRequest:
+      OnMapRequest(e.xmaprequest);
+      break;
+    case MapNotify:
+      OnMapNotify(e.xmap);
+    default:
+      LOG(WARNING) << "Ignored event";
+    }
+  }
+}
+
+void WindowManager::OnCreateNotify(const XCreateWindowEvent &e) {}
+
+void WindowManager::OnDestroyNotify(const XDestroyWindowEvent &e) {}
+
+void WindowManager::OnConfigureRequest(const XConfigureRequestEvent &e) {
+  XWindowChanges changes;
+  changes.x = e.x;
+  changes.y = e.y;
+  changes.width = e.width;
+  changes.height = e.height;
+  changes.border_width = e.border_width;
+  changes.sibling = e.above;
+  changes.stack_mode = e.detail;
+
+  if (clients_.count(e.window)) {
+    const Window frame = clients_[e.window];
+    XConfigureWindow(display_, frame, e.value_mask, &changes);
+    LOG(INFO) << "Resize [" << frame << "] to width: " << e.width
+              << " and height: " << e.height;
+  }
+
+  XConfigureWindow(display_, root_, e.value_mask, &changes);
+  LOG(INFO) << "Resize " << e.window << " to width: " << e.width
+            << " and height: " << e.height;
+}
+
+void WindowManager::OnConfigureNotify(const XConfigureEvent &e) {}
+
+void WindowManager::OnMapRequest(const XMapRequestEvent &e) {
+  Frame(e.window);
+
+  XMapWindow(display_, e.window);
+}
+
+void WindowManager::Frame(Window w) {
+  const unsigned int BORDER_WIDTH = 3;
+  const unsigned long BORDER_COLOR = 0xff0000;
+  const unsigned long BG_COLOR = 0x0000ff;
+
+  XWindowAttributes window_attr;
+  XGetWindowAttributes(display_, w, &window_attr);
+
+  const Window frame = XCreateSimpleWindow(
+      display_, root_, window_attr.x, window_attr.y, window_attr.width,
+      window_attr.height, BORDER_WIDTH, BORDER_COLOR, BG_COLOR);
+
+  XSelectInput(display_, frame,
+               SubstructureRedirectMask | SubstructureNotifyMask);
+
+  XAddToSaveSet(display_, w);
+
+  XReparentWindow(display_, w, frame, 0, 0);
+
+  XMapWindow(display_, frame);
+
+  clients_[w] = frame;
+
+  LOG(INFO) << "Framed window " << w << " [" << frame << "]";
+}
+
+void WindowManager::OnMapNotify(const XMapEvent &e) {}
+
+void WindowManager::OnReparentNotify(const XReparentEvent &e) {}
+
+int WindowManager::OnWMDectected(Display *display, XErrorEvent *e) {
+  CHECK_EQ(static_cast<int>(e->error_code), BadAccess);
+
+  wm_detected_ = true;
+
+  return 0;
+}
+
+int WindowManager::OnXError(Display *display, XErrorEvent *e) {
+  const int MAX_ERROR_TEXT_LENGTH = 1024;
+  char error_text[MAX_ERROR_TEXT_LENGTH];
+  XGetErrorText(display, e->error_code, error_text, sizeof(error_text));
+  LOG(ERROR) << static_cast<int>(e->error_code) << ":" << error_text;
+
+  return 0;
+}
--- src/wm/wm.hpp
+++ src/wm/wm.hpp
@@ -2,6 +2,7 @@ extern "C" {
 #include <X11/Xlib.h>
 }
 #include <memory>
+#include <unordered_map>
 
 class WindowManager {
 public:
@@ -12,9 +13,33 @@ public:
   void Run();
 
 private:
-  WindowManager(Display* display);
+  WindowManager(Display *display);
 
-  Display* display_;
+  Display *display_;
 
   const Window root_;
+
+  static inline bool wm_detected_ = false;
+
+  std::unordered_map<Window, Window> clients_;
+
+  static int OnXError(Display *display, XErrorEvent *e);
+
+  static int OnWMDectected(Display *display, XErrorEvent *e);
+
+  void OnCreateNotify(const XCreateWindowEvent &e);
+
+  void OnDestroyNotify(const XDestroyWindowEvent &e);
+
+  void OnConfigureRequest(const XConfigureRequestEvent &e);
+
+  void OnConfigureNotify(const XConfigureEvent &e);
+
+  void OnMapRequest(const XMapRequestEvent &e);
+
+  void OnMapNotify(const XMapEvent &e);
+
+  void OnReparentNotify(const XReparentEvent &e);
+
+  void Frame(Window w);
 };
